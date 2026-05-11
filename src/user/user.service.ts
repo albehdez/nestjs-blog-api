687858a23627ca714dd8c9b0a1a8d748cd2dbecc
Alberto Hdez <albertojhdez01@gmail.com>
2026-05-11 18:39:47 -0400

    add user feat

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
+import { UserEntity } from "@/user/user.entity";
+import { CreateUserDto } from "@/user/dto/createUser.dto";
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class UserService {
-    createUser(createUserDto) {
-        console.log(createUserDto);
-        
-        return createUserDto;
+    constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) {}
+    async createUser(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+        const newUser = new UserEntity()
+        Object.assign(newUser, createUserDto)
+        return await this.userRepository.save(newUser);
     }
 }
